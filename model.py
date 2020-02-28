68fb419d3a19c3976d98dd9349ffedbb91162da3
mahdirahbar <MahdiRahbar01@gmail.com>
2020-02-29 03:08:04 +0330

    added theinitial model file

---
+++ model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 29 03:05:26 2020
+
+@author: mahdi
+"""
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
